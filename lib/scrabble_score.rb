3eaa1aeeaa1119ef9458e2925a0eda910b8251d2
Alekseii Smirnov <alekseii.sm@gmail.com>
2020-12-28 23:25:47 +0200

    1 and 2 scores

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,3 +1,9 @@
+SCORES =  {
+  'a' => 1, 'e' => 1, 'i' => 1, 'o' => 1, 'u' => 1, 'l' => 1,
+  'n' => 1, 'r' => 1, 's' => 1, 't' => 1,
+  'd' => 2,'g' => 2
+}
+
 def scrabble_score symbol
-  1
+  return SCORES[symbol.downcase]
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -5,5 +5,23 @@ describe 'scrabble_score' do
   it 'returns 1 for a' do
     expect(scrabble_score('a')).to eq(1)
   end
+
+  it 'returns 2 for d' do
+    expect(scrabble_score('d')).to eq(2)
+  end
+
+  it 'returns correct values for upper case letters' do
+    expect(scrabble_score('A')).to eq(1)
+    expect(scrabble_score('D')).to eq(2)
+  end
+
+  it 'returns correct values for random 1 score letter' do 
+    expect(scrabble_score(%w[A E I O U L N R S T].sample)).to eq(1)
+  end 
   
+  it 'returns correct values for random 2 score letter' do 
+    expect(scrabble_score(%w[D G].sample)).to eq(2)
+  end 
+  
+ 
 end
